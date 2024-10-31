6092ac4c025ceb6e4a6a6dbef1280b53d5128e35
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-10-31 17:59:03 +0500

    bug fix in backend controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signUp } from "../controllers/userController";
+import { signUp } from "../controllers/userController.js";
 const router = express.Router();
 
 router.post("/register",signUp);
