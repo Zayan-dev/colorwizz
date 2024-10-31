e70403c129dec602782045adf388b319bf7d6c9a
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-10-31 17:50:12 +0500

    db name change in connectDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "ecommerce-db",
+      dbName: "colowizz",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
