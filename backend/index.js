9b7203547b27648f47e8ba00fb709bf50e2cf9be
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-10-31 17:42:05 +0500

    backend .env file created

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
@@ -19,3 +20,6 @@ connectDB();
 
 //user Routes
 app.use("/api", userRoutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
